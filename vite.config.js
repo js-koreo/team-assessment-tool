c70459eae10f7f7905cbd8f06c64a2d91b254e1b
js-koreo <jahnvi@koreo.co>
2025-03-05 23:28:05 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'build'
-  }
-});
+  base: '/team-assessment-tool/' // Must match your repository name
+})
